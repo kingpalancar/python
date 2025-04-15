044590b03f1cf869932fe45ab8d5f48b51773706
galicaea <bc248329@gmail.com>
2025-04-15 08:46:51 -0500

    Create clockulator.py
    combination clock and simple calculator

---
+++ clockulator.py
@@ -0,0 +1,90 @@
+#imports
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import Label, Tk
+from time import strftime
+
+#defs for calc and clock within doubleshell
+    #clock
+root = Tk()
+root.title("clockulator")
+root.geometry("300x500")
+root.configure(bg="white")
+
+def update_time():
+    current_time = strftime('%H:%M:%S %p')
+    current_date = strftime("""%A / %B / %d / %Y""")
+    time_label.config(text=current_time)
+    date_label.config(text=current_date)
+    root.after(1000, update_time)
+time_label = Label(root, font=('Garamond', 20, 'bold'), bg="white", fg="red")
+time_label.pack(anchor='center')
+date_label = Label(root, font=('Garamond', 10, 'bold'), bg='white',fg='red')
+date_label.pack(anchor='center')
+update_time()
+
+    #calc
+def addition():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        addition = num1 + num2
+        addition_label.config(text=f"Sum: {addition}", font=("Georgia", 12), fg="#000000")
+    except ValueError:
+        messagebox.showerror("Error", "need normal numbers", font=("Georgia", 12), fg="#000000")
+def subtraction():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        subtraction = num1 - num2
+        subtraction_label.config(text=f"Sub: {subtraction}", font=("Georgia", 12), fg="#000000")
+    except ValueError:
+        messagebox.showerror("Error", "need normal numbers", font=("Georgia", 12), fg="#000000")
+def multiplication():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        multiplication = num1 * num2
+        multiplication_label.config(text=f"Prod: {multiplication}", font=("Georgia", 12), fg="#000000")
+    except ValueError:
+        messagebox.showerror("Error", "need normal numbers", font=("Georgia", 12), fg="#000000")
+def division():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        division = num1 / num2
+        division_label.config(text=f"Quot: {division}", font=("Georgia", 12), fg="#000000")
+    except ValueError:
+        messagebox.showerror("Error", "need normal numbers", font=("Georgia", 12), fg="#000000")
+
+
+    #user input boxes (entryx.get definitions)
+tk.Label(root, text="First number:", bg="#ffffff", font=("Georgia", 12)).pack()
+entry1 = tk.Entry(root)
+entry1.pack()
+tk.Label(root, text="Second number:", bg="#ffffff", font=("Georgia", 12)).pack()
+entry2 = tk.Entry(root)
+entry2.pack()
+
+    #individual buttons for four operation definitions
+calculate_button = tk.Button(root, text="Add", font=("Georgia", 12), command=addition, bg="#ffffff", fg="#5ae129")
+calculate_button.pack(pady=10)
+calculate_button = tk.Button(root, text="Subtract", font=("Georgia", 12), command=subtraction, bg="#ffffff", fg="#5ae129")
+calculate_button.pack(pady=10)
+calculate_button = tk.Button(root, text="Divide", font=("Georgia", 12), command=division, bg="#ffffff", fg="#5ae129")
+calculate_button.pack(pady=10)
+calculate_button = tk.Button(root, text="Multiply", font=("Georgia", 12), command=multiplication, bg="#ffffff", fg="#5ae129")
+calculate_button.pack(pady=10)
+
+    #show individual outputs for each operation definition
+addition_label = tk.Label(root, bg="#ffffff", font=("Georgia", 12), text="Sum: ", fg="#000000")
+addition_label.pack()
+subtraction_label = tk.Label(root, bg="#ffffff", font=("Georgia", 12), text="Sub: ", fg="#000000")
+subtraction_label.pack()
+multiplication_label = tk.Label(root, bg="#ffffff", font=("Georgia", 12), text="Prod: ", fg="#000000")
+multiplication_label.pack()
+division_label = tk.Label(root, bg="#ffffff", font=("Georgia", 12), text="Quot: ", fg="#000000")
+division_label.pack()
+
+
+root.mainloop()
